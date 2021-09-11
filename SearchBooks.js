b1501eac2b4fa637b61d12c8b3341129fcca5e5f
Medeiros92 <thalesbehringer@gmail.com>
2021-09-11 11:49:37 -0300

    Array Declaration

--- SearchBooks.js
+++ SearchBooks.js
@@ -1,4 +1,40 @@
-
+//Declaração do array
+const booksByCategory = [
+    {
+        category: 'Riqueza',
+        books: [
+            {
+                title: 'Os segredos da mente milionária',
+                author: 'T. Harv Eker'
+            },
+            {
+                title: 'O homem mais rico da Babiônia',
+                author: 'George S. Classon'
+            },
+            {
+                title: 'Pai rico, pai pobre',
+                author: 'Robert T. Kiyosaki'
+            }
+        ]
+    },
+    {
+        category: 'Inteligência Emocional',
+        books: [
+            {
+                title: 'Você é Insubstituível',
+                author: 'Augusto Cury'
+            },
+            {
+                title: 'Ansiedade - Como enfrentar o mal do século',
+                author: 'Augusto Cury'
+            },
+            {
+                title: 'Os 7 hábitos das pessoas altamente eficazes ',
+                author: 'Stephen R. Covey'
+            }
+        ]
+    }
+];
 
 
 
